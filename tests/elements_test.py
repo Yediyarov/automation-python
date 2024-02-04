aa2311636b05acb379d1a17e40fd1b60f5460623
Yediyarov <yediyarov.xeyal@gmail.com>
2024-02-04 18:55:18 +0400

    add test for checkbox page

--- tests/elements_test.py
+++ tests/elements_test.py
@@ -1,4 +1,4 @@
-from pages.elements_page import TextBoxPage
+from pages.elements_page import CheckBoxPage, TextBoxPage
 
 class TestElements:
     class TestTextBox:
@@ -15,3 +15,13 @@ class TestElements:
             assert current_address == output_cur_addr, "the current address does not match"
             assert permanent_address == output_per_addr, "the permanent address does not match"
 
+    class TestCheckBox:
+        
+        def test_check_box(self, driver):
+            check_box_page = CheckBoxPage(driver, 'https://demoqa.com/checkbox')
+            check_box_page.open()
+            check_box_page.open_full_list()
+            check_box_page.click_random_checkbox()
+            input_checkbox = check_box_page.get_checked_checkboxes()
+            output_result = check_box_page.get_output_result()
+            assert input_checkbox == output_result, 'checkboxes have not been selected'
