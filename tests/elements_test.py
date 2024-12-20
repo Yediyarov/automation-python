bc99d1fb0cbb67d66395573499b1463d3bf21ee4
Yediyarov <yediyarov.xeyal@gmail.com>
2024-12-20 16:05:19 +0400

    add test for button appearance after five seconds

--- tests/elements_test.py
+++ tests/elements_test.py
@@ -74,3 +74,9 @@ class TestElements:
             dynamic_properties_page.open()
             color_before, color_after = dynamic_properties_page.check_changed_of_color()
             assert color_after != color_before, 'colors have not been changed'
+
+        def test_appear_button(self, driver):
+            dynamic_properties_page = DynamicPropertiesPage(driver, 'https://demoqa.com/dynamic-properties')
+            dynamic_properties_page.open()
+            appear = dynamic_properties_page.check_appear_of_button()
+            assert appear is True, 'button did not appear after 5 second'
