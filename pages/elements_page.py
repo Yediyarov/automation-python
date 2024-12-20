85eb22e0cbce85ae29d882750be036ca5f7ac3ea
Yediyarov <yediyarov.xeyal@gmail.com>
2024-12-20 15:55:07 +0400

    add method for button color changes

--- pages/elements_page.py
+++ pages/elements_page.py
@@ -1,3 +1,5 @@
+import time
+
 from selenium.common import TimeoutException
 
 from generator.generator import generated_person
@@ -95,3 +97,10 @@ class DynamicPropertiesPage(BasePage):
         except TimeoutException:
             return False
         return True
+
+    def check_changed_of_color(self):
+        color_button = self.element_is_present(self.locators.COLOR_CHANGE_BUTTON)
+        color_button_before = color_button.value_of_css_property('color')
+        time.sleep(5)
+        color_button_after = color_button.value_of_css_property('color')
+        return color_button_before, color_button_after
